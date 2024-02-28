152314ec7357ba84a8943cb54bc7a2a8e014189d
HaykArakelyan <hayk.arakelyan2002@gmail.com>
2024-02-28 17:07:52 +0400

    fix: Updated UI for input and icon

--- src/components/customComponents/CustomInputs.js
+++ src/components/customComponents/CustomInputs.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styles from "./Custominputs.module.css"
 import CustomIcon from './CustomIcon'
 
@@ -11,30 +11,39 @@ export default function CustomInputs({
     onIconClick = null,
     iconSize = null,
     iconStyle = null,
-    value
+    value,
+    parentStyle
 }) {
+
+    const inputRef = useRef(null);
 
     const handleIconClick = (e) => {
         e.stopPropagation()
         onIconClick()
     }
 
+    const handleKeyDown = (e) => {
+        if (e.key === 'Escape') {
+            inputRef.current.blur();
+        }
+    }
 
     return (
-        <div className={styles.container}>
+        <div className={styles.container} style={parentStyle}>
             <input
+                ref={inputRef}
                 type={type}
                 placeholder={placeholder}
                 className={styles.input}
                 onChange={(e) => onChange(e.target.value)}
                 style={style}
                 value={value}
-
+                onKeyDown={handleKeyDown}
             />
             {icon && onIconClick ?
                 <CustomIcon
                     icon={icon}
-                    onClick={(e) => handleIconClick(e)}
+                    onClick={handleIconClick}
                     size={iconSize}
                     style={iconStyle}
                 /> : null
