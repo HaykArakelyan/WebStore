398bd9bf87314f1a4a043fad096e5727191dc504
HaykArakelyan <hayk.arakelyan2002@gmail.com>
2024-02-29 15:51:43 +0400

    feat: Updated UI for 'My Products' page

--- src/pages/UserProducts/UserProducts.js
+++ src/pages/UserProducts/UserProducts.js
@@ -8,26 +8,69 @@ import CustomModal from '../../components/customComponents/CustomModal'
 
 import tempImage from './temp-image.png'//TODO: Temp
 import ProductForm from '../../components/Product/ProductForm'
+import PaginationControl from '../../components/PaginationControl/PaginationControl'
 
 import { isNullOrUndefined } from '../../CustomTools/CustomTools'
-import { delete_user_product, edit_product, get_products_by_userId } from '../../CustomTools/Requests'
+import { delete_user_product, edit_product, get_products_by_userId, add_product } from '../../CustomTools/Requests'
 import Product from '../../components/Product/Product'
+import CustomInputs from '../../components/customComponents/CustomInputs'
+import { faX } from '@fortawesome/free-solid-svg-icons';
+import { filteredProducts } from '../../CustomTools/CustomTools'
+import Loader from '../../components/Loader/Loader'
+import CustomButton from '../../components/customComponents/CustomButton'
+
+
+const userId = sessionStorage.getItem("id")
 
 export default function UserProducts() {
-    const location = useLocation()
-    const userId = sessionStorage.getItem("id")
 
     const [products, setProducts] = useState([])
+    const [filteredProductList, setFilteredProductList] = useState([])
+    const [searchQuery, setSearchQuery] = useState("")
+
+    const [currentPage, setCurrentPage] = useState(0)
+    const ITEMS_PER_PAGE = 12
+
+    const [errorMessege, setErrorMessege] = useState("")
+    const [isFetching, setIsFethcing] = useState(true)
+    const [isNewProductAdded, setIsNewProductAdded] = useState(false)
+
+    useEffect(() => {
+        if (isFetching) {
+            setErrorMessege(<Loader />)
+        }
+    }, [isFetching])
+
 
     useEffect(() => {
         get_products_by_userId(userId)
-            .then((res) =>
+            .then((res) => {
                 setProducts(res.products)
-            )
-            .catch((err) => {
-                console.log(err)
+                setFilteredProductList(res.products)
             })
-    }, [])
+            .catch((err) => {
+                if (err.code === "ERR_NETWORK") {
+                    setErrorMessege("No Internet Connection")
+                } else {
+                    setErrorMessege("Something Went Wrong")
+                }
+            })
+            .finally(() => {
+                setIsFethcing(false)
+                setIsNewProductAdded(false)
+            })
+    }, [isNewProductAdded])
+
+
+    useEffect(() => {
+        const result = filteredProducts(products, searchQuery)
+        if (result.length === 0 && products.length !== 0) {
+            setErrorMessege(`No Product Found With "${searchQuery}"`)
+        }
+        setFilteredProductList(result)
+        setCurrentPage(0)
+    }, [products, searchQuery])
+
 
     const [isModalHidden, setIsModalHidden] = useState(true);
     const [activeProduct, setActiveProduct] = useState(null);
@@ -113,18 +156,79 @@ export default function UserProducts() {
         })
     }
 
+    const handleAddproductButtonClick = () => {
+        setModalElement(
+            <ProductForm
+                onSubmit={handlePostProductButtonClick}
+                newProduct
+            />)
+        setIsModalHidden(false)
+    }
+
+    const handlePostProductButtonClick = (e) => {
+        add_product(e)
+            .then((res) => {
+                setIsModalHidden(true)
+                setIsNewProductAdded(true)
+            }).catch((err) => {
+                console.log(err)
+            })
+    }
+
+    const startIndex = currentPage * ITEMS_PER_PAGE;
+    const endIndex = startIndex + ITEMS_PER_PAGE;
+    const currentData = filteredProductList.slice(startIndex, endIndex);
+
     return (
         <div className={styles.container}>
-            {products.map((product, i) =>
-                <CustomCard
-                    key={i}
-                    //TODO: Get Image URLs from Database
-                    p={{ ...product, images: [tempImage] }}
-                    onClick={() => handleCardClick(product, i)}
+            <div className={styles.controls}>
+
+                <div className={styles.addProduct}>
+                    <CustomButton
+                        text={"Add Product"}
+                        onClick={handleAddproductButtonClick}
+                    />
+                </div>
+
+                <div className={styles.searchBox}>
+                    <CustomInputs
+                        placeholder={"Search"}
+                        onChange={setSearchQuery}
+                        value={searchQuery}
+                        icon={faX}
+                        onIconClick={() => setSearchQuery("")}
+                        parentStyle={{ gap: "1rem" }}
+                        iconStyle={{ padding: " 0.5rem .7rem 0.5rem" }}
+
+                    />
+                </div>
+            </div>
+            <div className={styles.userProducts}>
+                {currentData.map((product, i) =>
+                    <CustomCard
+                        key={i}
+                        //TODO: Get Image URLs from Database
+                        p={{ ...product, images: [tempImage] }}
+                        onClick={() => handleCardClick(product, i)}
+                    />
+
+                )}
+            </div>
+
+            {filteredProductList.length !== 0 ?
+                Math.ceil(filteredProductList.length / ITEMS_PER_PAGE) !== 1 &&
+                <PaginationControl
+                    productsLength={products.length}
+                    itemsPerPage={ITEMS_PER_PAGE}
+                    currentPage={currentPage}
+                    setCurrentPage={setCurrentPage}
+                    filteredProductList={filteredProductList}
                 />
-
-            )}
-
+                :
+                <div className={styles.productsError}>
+                    <label>{errorMessege}</label>
+                </div>
+            }
             <AnimatePresence
                 initial={false}
                 mode='wait'
